2d2301e40c08559674319367e55d3d40963402bd
SimonBao <Simonbao.sb@gmail.com>
2018-02-10 16:03:48 +0000

    BowlingGame  * Now calculates score from frames

--- src/BowlingGame.js
+++ src/BowlingGame.js
@@ -9,17 +9,30 @@ BowlingGame.prototype.score = function () {
     return this.gameScore;
 };
 
-BowlingGame.prototype.roll = function (pins) {
-    this.currentFrame.rolls.push(pins);
-
-};
+// BowlingGame.prototype.roll = function (pins) {
+//     this.currentFrame.rolls.push(pins);
+//
+// };
 
 BowlingGame.prototype._calculateScore = function () {
     for(var i = 0; i < this.frames.length; i++){
-        this.gameScore += this.frames[i];
+        this.gameScore += this.frames[i].score();
     }
 };
 
 
 
+BowlingGame.prototype.roll = function (pins) {
+    if(this.currentFrame.rolls.length < 2){
+        this.currentFrame.rolls.push(pins);
+        if(this.currentFrame.rolls.length === 2){
+            this.frames.push(this.currentFrame);
+            this.currentFrame = new Frame();
+        }
+    } else {
+        this.frames.push(this.currentFrame);
+        this.currentFrame = new Frame();
+        this.currentFrame.rolls.push(pins)
+    }
+};
 
